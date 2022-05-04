ae7228d331b3846b768c83fb7398072c9c89a146
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2022-05-04 17:46:50 +0200

    jour 4

---
+++ jour4/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="script.js"></script>
+</head>
+<body>
+    <button id="button"></button>
+</body>
+</html>
